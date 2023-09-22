f4f864bdd5361d5c1a20f9576b7b8a1772c99e28
ferenccc <nadjferenc00@gmail.com>
2023-09-23 00:18:27 +0200

    Hibajavítások és megjegyzések hozzáadva

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+constexpr int N_ELEMENTS = 100;
+
+int main()
+{
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++)
+    {
+        b[i] = i * 2;
+    }
+    std::cout << "Ertek: ";
+    for (int i = 0; i < N_ELEMENTS; i++)
+    {
+        std::cout << b[i] << " ";
+    }    
+    std::cout << "\nAtlag szamitasa: " << std::endl;
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
+    {
+        atlag += b[i];
+    }
+    atlag /= N_ELEMENTS;
+    std::cout << "Atlag: " << atlag << std::endl;
+
+    delete[] b; // Ne felejtse el felszabadítani a dinamikusan foglalt memóriát
+
+    return 0;
+}
